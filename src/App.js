e89e416b02d511d71a3eb082b115aaa135a85173
AKIN-THOMAS <104861747+AKIN-THOMAS@users.noreply.github.com>
2023-10-01 19:21:37 +0100

    commit one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import DashLayout from "./components/dashboard/DashLayout";
+import DashHome from "./pages/DashHome";
+import VideoDetails from "./components/dashboard/VideoDetails";
+import ErrorMessage from "./components/ErrorMessage";
+import Login from "./components/main/Login";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route indexed element={<DashLayout />}>
+          <Route path="/home" element={<DashHome />} />
+          <Route path="/home/:id" element={<VideoDetails />} />
+          <Route
+            path="*"
+            element={<ErrorMessage message={"Page not found"} />}
+          />
+        </Route>
+        <Route path="*" element={<ErrorMessage message={"Page not found"} />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
